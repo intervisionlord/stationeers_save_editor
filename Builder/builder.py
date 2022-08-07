a7b09070ebe8bdbad9e8b520f6feb8e2fc7361a9
intervisionlord <ads-net@mail.ru>
2022-08-07 23:03:12 +0300

    Незначительные правки сборщика

--- Builder/builder.py
+++ Builder/builder.py
@@ -2,7 +2,12 @@ import zipfile
 import yaml
 import os
 
-def getconfig():
+def getconfig() -> dict:
+    """Загружает данные из конфига сборки.
+
+    Returns:
+        dict: Список переменных конфига
+    """
     try:
         with open('./config.yaml', 'r') as confFile:
             conf = yaml.full_load(confFile)
@@ -11,11 +16,12 @@ def getconfig():
     return conf
 
 def zipOutput():
+    """Упаковывает готовый файл в архив.
+    """
     with zipfile.ZipFile('SSE.zip', 'w',
                          compression=zipfile.ZIP_DEFLATED,
                          compresslevel=9) as zipArch:
         zipArch.write('SSE.exe')
-
 
 conf = getconfig()
 paramsStr = '--' + ' --'.join(conf['params'])
@@ -23,5 +29,7 @@ plugins = conf['plugins']
 icon = conf['main']['icon']
 
 if __name__ == '__main__':
+    """Запуск сборки бинарного файла и его упаковка в архив.
+    """
     os.system(f'nuitka {paramsStr} --plugin-enable={plugins} --windows-icon-from-ico={icon} ../SSE.py')
     zipOutput()
